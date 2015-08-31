eff163856704502d2378f3d05885f6f6c5070321
Edson Alves <edsonalves@unb.br>
2015-08-31 09:14:54 -0300

    Adição da estruturas UFDS.

---
+++ algorithms/data_structures/union_find.h
@@ -0,0 +1,74 @@
+/**
+ * Classe UDFS (Union-Find Disjoint Sets)
+ *
+ * Autor: Edson Alves
+ * Data: 31/08/2015
+ * Licença: LGPL. Sem copyright.
+ */
+#ifndef UNION_FIND_H
+#define UNION_FIND_H
+
+#define UDFS_MAX_N 1000005
+
+int parent[UDFS_MAX_N];
+int rank[UDFS_MAX_N];
+
+class UnionFind
+{
+public:
+    UnionFind(int N)
+        : m_N(N), m_count(N)
+    {
+        for (int i = 1; i <= N; ++i)
+        {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+    }
+
+    bool is_same_set(int i, int j)
+    {
+        return find_set(i) == find_set(j);        
+    }
+
+    void union_set(int i, int j)
+    {
+        if (is_same_set(i, j))
+            return;
+
+        int p = find_set(i);
+        int q = find_set(j);
+
+        if (rank[p] > rank[q])
+            parent[q] = p;
+        else if (rank[q] > rank[p])
+            parent[p] = q;
+        else 
+        {
+            parent[q] = p;
+            ++rank[p];
+        }
+
+        --m_count;
+    }
+
+    int count() const { return m_count; }
+
+    int find_set(int i)
+    {
+        int p = i;
+
+        while (p != parent[p])
+            p = parent[p];
+
+        parent[i] = p;
+
+        return p;
+    }
+
+private:
+    int m_N;
+    int m_count;
+};
+
+#endif
